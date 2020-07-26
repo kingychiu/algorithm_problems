4a6b5484e79c7053bb12eadf27135b8651ad4acb
Anthony Chiu <kingychiu@gmail.com>
2020-07-26 12:53:54 +0800

    Add Rotting Oranges

---
+++ src/rotting_oranges/solution.py
@@ -0,0 +1,90 @@
+"""
+    https://leetcode.com/problems/rotting-oranges/
+"""
+from typing import List, Tuple
+
+
+class Solution:
+    # pylint: disable=too-few-public-methods
+    """
+        Solution
+    """
+
+    def oranges_rotting(self, grid: List[List[int]]):
+        # pylint: disable=no-self-use too-many-locals too-many-branches
+        """
+            In a given grid, each cell can have one of three values:
+
+            the value 0 representing an empty cell;
+            the value 1 representing a fresh orange;
+            the value 2 representing a rotten orange.
+            Every minute, any fresh orange that is adjacent (4-directionally)
+            to a rotten orange becomes rotten.
+
+            Return the minimum number of minutes that must elapse until no cell has a fresh orange.
+              If this is impossible, return -1 instead.
+        """
+        if len(grid) == 0:
+            return 0
+
+        # find out all initial rotten orange and
+        # for each minutes elapsed, propagate the rotten status
+        grid_height = len(grid)
+        grid_width = len(grid[0])
+        # Use BFS to start level-wise exploration from all initial rotten orange
+        visited = {}
+        # with a queue of tuples (row, col, minute_took)
+        queue: List[Tuple[int, int, int]] = []
+        num_fresh_orange = 0
+
+        for row in range(grid_height):
+            visited[row] = {}
+            for col in range(grid_width):
+                visited[row][col] = False  # initialize the BFS visited dict
+                if grid[row][col] == 2:  # Add the initial rotten orange to the BFS queue
+                    queue.append((row, col, 0))
+                if grid[row][col] == 1:  # initialize the fresh orange counter
+                    num_fresh_orange += 1
+
+        # if there are no fresh orange in the initial setup, return 0
+        if num_fresh_orange == 0:
+            return 0
+
+        total_minute_took = 0
+        while queue:
+            # BFS dequeue
+            row, col, minute_took = queue.pop(0)
+            # update time
+            total_minute_took = minute_took
+
+            # filter all possible next moves
+            next_nodes = []
+            can_go_up = row > 0
+            can_go_down = row < grid_height - 1
+            can_go_left = col > 0
+            can_go_right = col < grid_width - 1
+
+            if can_go_up:
+                next_nodes.append((row - 1, col))
+            if can_go_down:
+                next_nodes.append((row + 1, col))
+            if can_go_left:
+                next_nodes.append((row, col - 1))
+            if can_go_right:
+                next_nodes.append((row, col + 1))
+
+            for nrow, ncol in next_nodes:
+                # check visited or not
+                if visited[nrow][ncol]:
+                    continue
+                visited[nrow][ncol] = True
+
+                # Only move to fresh orange
+                if grid[nrow][ncol] == 1:
+                    num_fresh_orange -= 1
+                    # BFS enqueue
+                    queue.append((nrow, ncol, minute_took + 1))
+
+        if num_fresh_orange == 0:
+            return total_minute_took
+        return -1
---
+++ src/rotting_oranges/test_solution.py
@@ -0,0 +1,21 @@
+""" Testing solution """
+# pylint: disable=duplicate-code
+import pytest
+
+from .solution import Solution
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ([], 0),
+        ([[2, 1, 1], [1, 1, 0], [0, 1, 1]], 4),
+        ([[2, 1, 1], [0, 1, 1], [1, 0, 1]], -1),
+        ([[0, 2]], 0),
+    ],
+)
+def test_solution(test_input, expected):
+    """
+        Checking Test Cases
+    """
+    assert Solution().oranges_rotting(test_input) == expected
