fe101788403e19b5e8e2c78aac88b8d75a75eb25
Anthony Chiu <kingychiu@gmail.com>
2021-02-26 02:01:18 +0800

    Refactor

--- src/maximum_number_of_visible_points/solution.py
+++ src/maximum_number_of_visible_points/solution.py
@@ -13,16 +13,6 @@ class Solution:
         Solution
     """
 
-    @staticmethod
-    def _get_angle(x: int, y: int) -> float:
-        """
-            Return the angle from the positive x-axis counter-clockwise
-        """
-        radian = math.atan2(y, x)
-        if radian < 0:
-            radian = 2 * math.pi + radian
-        return math.degrees(radian)
-
     def visible_points(self, points: List[List[int]], angle: int, location: List[int]) -> int:
         # pylint: disable=no-self-use
         """
@@ -30,9 +20,6 @@ class Solution:
         """
         num_points = len(points)
         num_always_visible = 0
-
-        if num_points == 0:
-            return 0
 
         point_angles = []
         for i in range(num_points):
@@ -46,38 +33,34 @@ class Solution:
                 continue
 
             # Convert all points to angles from the positive x-axis counter-clockwise
-            point_angle = self._get_angle(new_x, new_y)
+            radian = math.atan2(new_y, new_x)
+            if radian < 0:
+                radian = 2 * math.pi + radian
+
+            point_angle = math.degrees(radian)
             point_angles.append(point_angle)
             # For circular loop
             point_angles.append(360 + point_angle)
 
         # Sort the points by their angle
-        point_angles = sorted(point_angles)
+        point_angles.sort()
 
         if len(point_angles) == 0:
             return num_always_visible
 
-        from_idx = 0
-        to_idx = 0
-
-        num_visible = 0
-        max_visible = num_visible
+        start = 0
+        end = 0
+        max_visible = 0
 
         while True:
-            # record
-            num_visible = to_idx - from_idx + 1
-            max_visible = max(max_visible, num_visible)
-
-            if point_angles[from_idx] >= 360:
+            if point_angles[start] >= 360:
                 break
 
-            # check next
-            next_angle = point_angles[to_idx + 1] - point_angles[from_idx]
-            if next_angle > angle:
-                # not valid
-                from_idx += 1
-            else:
-                # valid
-                to_idx += 1
+            end += 1
+            if point_angles[end] - point_angles[start] > angle:
+                start += 1
+            
+            # record
+            max_visible = max(max_visible, end - start + 1)
 
         return max_visible + num_always_visible
