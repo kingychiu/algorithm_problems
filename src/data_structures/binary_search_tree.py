06efe797b57c9f3aafa3aee560b73b3fb0eec2e5
Anthony Chiu <kingychiu@gmail.com>
2022-05-22 21:17:27 +0800

    Standard BST insert

--- src/data_structures/binary_search_tree.py
+++ src/data_structures/binary_search_tree.py
@@ -128,3 +128,44 @@ def levelorder(node: BinarySearchTreeNode):
             results.append((curr_node.right.val, curr_node.right.count))
             q.put(curr_node.right)
     return results
+
+
+def insert(root: BinarySearchTreeNode, val: int):
+    """Standard BST Insertion"""
+    # Left Value < Root Value < Right Vale
+    q: Queue = Queue()
+    q.put(root)
+
+    while not q.empty():
+        curr = q.get()
+        # Compare the value with the root value
+        if curr.val == val:  # Increment the counter
+            curr.count += 1
+            return curr
+        elif val < curr.val:  # Go left
+            if curr.left is not None:
+                q.put(curr.left)
+            else:
+                curr.left = BinarySearchTreeNode(val)
+                return curr.left
+        else:  # Go right
+            if curr.right:
+                q.put(curr.right)
+            else:
+                curr.right = BinarySearchTreeNode(val)
+                return curr.right
+
+
+def avl_insert(root: BinarySearchTreeNode, val: int):
+    """
+    AVL tree is a self-balancing Binary Search Tree (BST) where the difference between
+    heights of left and right subtrees cannot be more than one for all nodes.
+    """
+    # node_w = insert(root, val)
+
+
+def avl_delete():
+    """
+    AVL tree is a self-balancing Binary Search Tree (BST) where the difference between
+    heights of left and right subtrees cannot be more than one for all nodes.
+    """
--- src/data_structures/tests/test_binary_searh_tree.py
+++ src/data_structures/tests/test_binary_searh_tree.py
@@ -4,51 +4,97 @@
 from src.data_structures import binary_search_tree
 
 
-def test_tree_1():
+def test_tree_traversal():
     """
     Test Binary Search Tree
-               1
+               8
              /   \
             /     \
-           2       3
-          /      /   \
-         /      /     \
-        4      5       6
-              / \
-             /   \
-            7     8
+           3       10
+          / \       \
+         /   \       \
+        1     6       14
+             / \     /
+            /   \   /
+           4     7 13
     """
 
-    root = binary_search_tree.BinarySearchTreeNode(1)
-    root.left = binary_search_tree.BinarySearchTreeNode(2)
-    root.left.left = binary_search_tree.BinarySearchTreeNode(4)
+    root = binary_search_tree.BinarySearchTreeNode(8)
+    root.left = binary_search_tree.BinarySearchTreeNode(3)
+    root.left.left = binary_search_tree.BinarySearchTreeNode(1)
+    root.left.right = binary_search_tree.BinarySearchTreeNode(6)
+    root.left.right.left = binary_search_tree.BinarySearchTreeNode(4)
+    root.left.right.right = binary_search_tree.BinarySearchTreeNode(7)
 
-    root.right = binary_search_tree.BinarySearchTreeNode(3)
-    root.right.right = binary_search_tree.BinarySearchTreeNode(6)
-    root.right.left = binary_search_tree.BinarySearchTreeNode(5)
-    root.right.left.left = binary_search_tree.BinarySearchTreeNode(7)
-    root.right.left.right = binary_search_tree.BinarySearchTreeNode(8)
+    root.right = binary_search_tree.BinarySearchTreeNode(10)
+    root.right.right = binary_search_tree.BinarySearchTreeNode(14)
+    root.right.right.left = binary_search_tree.BinarySearchTreeNode(13)
 
     results_recursive = binary_search_tree.inorder_recursive(root)
     results_iterative = binary_search_tree.inorder_iterative(root)
 
     for results in [results_recursive, results_iterative]:
-        assert [result[0] for result in results] == [4, 2, 1, 7, 5, 8, 3, 6]
+        assert [result[0] for result in results] == [1, 3, 4, 6, 7, 8, 10, 13, 14]
         assert [result[1] for result in results] == [1] * len(results)
 
     results_recursive = binary_search_tree.preorder_recursive(root)
     results_iterative = binary_search_tree.preorder_iterative(root)
 
     for results in [results_recursive, results_iterative]:
-        assert [result[0] for result in results] == [1, 2, 4, 3, 5, 7, 8, 6]
+        assert [result[0] for result in results] == [8, 3, 1, 6, 4, 7, 10, 14, 13]
         assert [result[1] for result in results] == [1] * len(results)
 
     results_recursive = binary_search_tree.postorder_recursive(root)
 
     for results in [results_recursive]:
-        assert [result[0] for result in results] == [4, 2, 7, 8, 5, 6, 3, 1]
+        assert [result[0] for result in results] == [1, 4, 7, 6, 3, 13, 14, 10, 8]
         assert [result[1] for result in results] == [1] * len(results)
 
     results = binary_search_tree.levelorder(root)
-    assert [result[0] for result in results] == [1, 2, 3, 4, 5, 6, 7, 8]
+    assert [result[0] for result in results] == [8, 3, 10, 1, 6, 14, 4, 7, 13]
     assert [result[1] for result in results] == [1] * len(results)
+
+
+def test_tree_insert():
+    """
+    Test Binary Search Tree
+               8
+             /   \
+            /     \
+           3       10
+          / \       \
+         /   \       \
+        1     6       14
+             / \     /
+            /   \   /
+           4     7 13
+    """
+
+    root = binary_search_tree.BinarySearchTreeNode(8)
+    root.left = binary_search_tree.BinarySearchTreeNode(3)
+    root.left.left = binary_search_tree.BinarySearchTreeNode(1)
+    root.left.right = binary_search_tree.BinarySearchTreeNode(6)
+    root.left.right.left = binary_search_tree.BinarySearchTreeNode(4)
+    root.left.right.right = binary_search_tree.BinarySearchTreeNode(7)
+
+    root.right = binary_search_tree.BinarySearchTreeNode(10)
+    root.right.right = binary_search_tree.BinarySearchTreeNode(14)
+    root.right.right.left = binary_search_tree.BinarySearchTreeNode(13)
+
+    binary_search_tree.insert(root, 0)
+    results = binary_search_tree.inorder_recursive(root)
+    assert [result[0] for result in results] == [0, 1, 3, 4, 6, 7, 8, 10, 13, 14]
+
+    binary_search_tree.insert(root, 9)
+    results = binary_search_tree.inorder_recursive(root)
+    assert [result[0] for result in results] == [0, 1, 3, 4, 6, 7, 8, 9, 10, 13, 14]
+
+    binary_search_tree.insert(root, 1)
+    results = binary_search_tree.inorder_recursive(root)
+    assert [result[0] for result in results] == [0, 1, 3, 4, 6, 7, 8, 9, 10, 13, 14]
+    assert root.left.left.count == 2
+
+    binary_search_tree.insert(root, 15)
+    results = binary_search_tree.inorder_recursive(root)
+    assert [result[0] for result in results] == [0, 1, 3, 4, 6, 7, 8, 9, 10, 13, 14, 15]
+    assert root.left.left.count == 2
