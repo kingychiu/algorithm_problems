12bd47d45e931368145cc94e473dada628587328
Anthony Chiu <kingychiu@gmail.com>
2022-05-23 11:34:09 +0800

    Refactor maximum_depth_of_n_ary_tree

--- src/maximum_depth_of_n_ary_tree/solution.py
+++ src/maximum_depth_of_n_ary_tree/solution.py
@@ -1,8 +1,8 @@
 """
     https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
 """
-from typing import List, Tuple, Optional
-
+from typing import Optional
+from collections import deque
 from src.data_structures.leetcode.tree import Node
 
 
@@ -21,16 +21,19 @@ class Solution:
             return 0
 
         # Setting up a queue for BFS with item = (node, node_level)
-        queue: List[Tuple[Node, int]] = [(root, 1)]
-        max_level: int = 1
+        queue: deque[Node] = deque([root])
+        max_level: int = 0
         while queue:
-            # de-queue
-            curr_node, curr_level = queue.pop(0)
+            next_queue: deque[Node] = deque()
+            while queue:
+                # de-queue
+                curr_node = queue.popleft()
+                if curr_node.children is not None:
+                    for child in curr_node.children:
+                        # en-queue the child node and add 1 to the level
+                        next_queue.append(child)
             # update maximum level
-            max_level = max(curr_level, max_level)
+            max_level += 1
+            queue = next_queue
 
-            if curr_node.children is not None:
-                for child in curr_node.children:
-                    # en-queue the child node and add 1 to the level
-                    queue.append((child, curr_level + 1))
         return max_level
