47144cb6f61bead42543226c1901bb494791fbde
Anthony Chiu <kingychiu@gmail.com>
2023-08-20 20:02:46 +0800

    Fix lint

--- src/q605_easy_can_place_flowers/solution.py
+++ src/q605_easy_can_place_flowers/solution.py
@@ -8,11 +8,11 @@
 # @lc code=start
 class Solution:
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
-        l = len(flowerbed)
-        for i in range(l):
+        num_beds = len(flowerbed)
+        for i in range(num_beds):
             spot_empty = flowerbed[i] == 0
             left_empty = i == 0 or flowerbed[i - 1] == 0
-            right_empty = i == (l - 1) or flowerbed[i + 1] == 0
+            right_empty = i == (num_beds - 1) or flowerbed[i + 1] == 0
             if spot_empty and left_empty and right_empty:
                 n -= 1
                 flowerbed[i] = 1
