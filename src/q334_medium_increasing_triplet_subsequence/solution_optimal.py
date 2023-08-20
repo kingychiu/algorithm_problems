68665c582b23f485afba8019566043ceb9f7814c
Anthony Chiu <kingychiu@gmail.com>
2023-08-21 01:13:06 +0800

    Update note

--- src/q334_medium_increasing_triplet_subsequence/solution_optimal.py
+++ src/q334_medium_increasing_triplet_subsequence/solution_optimal.py
@@ -15,7 +15,7 @@ class Solution:
         second_smallest = float("+inf")
         for num in nums:
             """
-            if the triplet values are truely monotonic increasing,
+            if the triplet values are truly monotonic increasing,
             num will match the the cases below 1 by 1.
             EX: 1, 2, 3
             1: matches 1st
@@ -27,6 +27,14 @@ class Solution:
             3: matches 2nd
             2: matches 2nd (replace 3)
             4" matches 3rd
+            
+            At the end the first, second, third values here MIGHT NOT BE the triplet we are looking for.
+            For example with 20,100,10,12,5,13. the final values are 5,12,13.
+            But because of the 2nd value is 12, we can ensure every first second smallest values are also smaller than 12.
+            
+            Generally speaking,
+            - Every historical first_smallest must smaller than the latest second_smallest
+            - Every historical second_smallest must smaller than the latest third (returned already).
             """
             if num <= first_smallest:
                 first_smallest = num
